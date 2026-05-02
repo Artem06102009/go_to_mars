00705dd2248c24e71300f3ffaffc06a32c23ea86
artemkolegov <kolegov_ai@mail.ru>
2026-05-02 22:44:15 +0300

    1 commit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, url_for, redirect
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField, BooleanField, SubmitField
-# from wtforms.validators import DataRequired
 from loginform import LoginForm
 
 app = Flask(__name__)
